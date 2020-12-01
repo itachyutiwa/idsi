8f4857648b94ca87356bd10273dcb475047738cd
Your Name <you@example.com>
2020-12-01 12:05:04 +0100

    Update requirements.txt

--- serverpjt/prod_settings.py
+++ serverpjt/prod_settings.py
@@ -1,6 +1,10 @@
 from serverpjt.settings import *
+import dj_database_url
+
 DEBUG =False
 TEMPLATES_DEBUG = False
+DATABASES['default'] = dj_database_url.config()
 SECRET_KEY = '-n(jxc8!_n@gg!kq$nvnyp1=em2l3rg=ib)-al7($*d*ofs6om'
 ALLOWED_HOSTS = ['idsi.herokuapp.com']
+
 
