a8dcf90b216e2b14e21819442a145e1d8b7b1eb5
Your Name <you@example.com>
2020-12-01 10:58:36 +0100

    Nouvelle modification pour resoudre le problème depython manage.py collectstatic --noinput

--- serverpjt/settings.py
+++ serverpjt/settings.py
@@ -124,11 +124,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
