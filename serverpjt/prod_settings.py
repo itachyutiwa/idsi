489ecc274ca54a99cf6684ddc56470ce74c7ea8e
Your Name <you@example.com>
2020-12-01 11:27:30 +0100

    Ajout de prod_settings.py et la variable d'environnement DJANGO_SETTINGS_MODULE à Heroku

---
+++ serverpjt/prod_settings.py
@@ -0,0 +1,6 @@
+from serverpjt.settings import *
+DEBUG =False
+TEMPLATES_DEBUG = False
+SECRET_KEY = '-n(jxc8!_n@gg!kq$nvnyp1=em2l3rg=ib)-al7($*d*ofs6om'
+ALLOWED_HOSTS = ['idsi.herokuapp.com']
+
--- serverpjt/settings.py
+++ serverpjt/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p*_hy!)7t6tmbdp!-1_w$#edlcvkz)s3ggaj#_9kf9q6*%vc(v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['idsi.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
