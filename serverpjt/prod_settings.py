db69c1e3cf3ab2412315b5b2921bbb5b57cd8368
Your Name <you@example.com>
2020-12-01 13:22:17 +0100

    Ajout de la configuration de whitenoise

--- serverpjt/prod_settings.py
+++ serverpjt/prod_settings.py
@@ -5,6 +5,8 @@ DEBUG =False
 TEMPLATES_DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 SECRET_KEY = '-n(jxc8!_n@gg!kq$nvnyp1=em2l3rg=ib)-al7($*d*ofs6om'
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ALLOWED_HOSTS = ['idsi.herokuapp.com']
 
 
